0a6cc9c025fbe1d2573237d31a3e5b3a9eb3ea8d
Hunter M <hunterm.haxxr@gmail.com>
2012-08-28 20:23:00 -0400

    actually fixed tabs now

--- config.example.php
+++ config.example.php
@@ -1,13 +1,13 @@
 <?php
-	$debug = false;
-	$mysqlserver = 'localhost:3306';
-	$database = 'minecraft'; //The same as in LB config.
-	// Change lb-main to be your world name, likely lb-world
-	$tables = array('lb-main'); //List of all tables that should get summed up
-	$user = 'mysqlusername';
-	$password = 'hunter2';
-	$cooldown = 10; //Time in seconds between to queries, to prevent spamming of expensive sql queries.
-	$excludedPlayers = array('Fire', 'TNT', 'LavaFlow', 'Creeper', 'Ghast', 'LeavesDecay', 'WaterFlow', 'SnowForm', 'SnowFade', 'NaturalGrow');
-	include 'lang_en.php'; //Language file
-	include 'materials_en.php'; //Block names file
+    $debug = false;
+    $mysqlserver = 'localhost:3306';
+    $database = 'minecraft'; //The same as in LB config.
+    // Change lb-main to be your world name, likely lb-world
+    $tables = array('lb-main'); //List of all tables that should get summed up
+    $user = 'mysqlusername';
+    $password = 'hunter2';
+    $cooldown = 10; //Time in seconds between to queries, to prevent spamming of expensive sql queries.
+    $excludedPlayers = array('Fire', 'TNT', 'LavaFlow', 'Creeper', 'Ghast', 'LeavesDecay', 'WaterFlow', 'SnowForm', 'SnowFade', 'NaturalGrow');
+    include 'lang_en.php'; //Language file
+    include 'materials_en.php'; //Block names file
 ?>
